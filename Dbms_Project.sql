30d165cc4c4498156bd304edd98c4c517584c6bb
Khushi Bansal <91721407+Khushibansal15@users.noreply.github.com>
2023-12-11 17:09:23 +0530

    Update Dbms_Project.sql

--- Dbms_Project.sql
+++ Dbms_Project.sql
@@ -1,272 +1,301 @@
-REM   Script: dbms3
-REM   DBMS PROJECT
+REM   Script: project
+REM   ...
 
-CREATE TABLE TAXI ( 
-   Taxi_id INT NOT NULL, 
-   Registration_no VARCHAR(20), 
-   Taxi_Model VARCHAR(20), 
-   Taxi_Year DATE, 
-   Taxi_type VARCHAR(20), 
-   Status VARCHAR(20), 
-   Driver_id INT, 
-   PRIMARY KEY (Taxi_id), 
-   UNIQUE (Registration_no) 
-);
+CREATE TABLE College(CollegeID VARCHAR(255) NOT NULL, CollegeName VARCHAR(255), CollegeAddress VARCHAR(255), CollegeEmail VARCHAR(255), CollegeStudentCount int, PRIMARY KEY(CollegeID));
 
-CREATE TABLE  USER_TBL ( 
-   Usr_id INT NOT NULL, 
-   F_name VARCHAR(20), 
-   L_name VARCHAR(20), 
-   Contat_no INT, 
-   Gender VARCHAR(10), 
-   Address VARCHAR(50), 
-   Taxi_id INT, 
-   PRIMARY KEY (Usr_id) 
-);
+insert into College values('c101','TU','Patiala','tu@gmail.com','2500');
 
-CREATE TABLE   DRIVER ( 
-   Driver_id INT NOT NULL, 
-   F_name VARCHAR(10), 
-   L_name VARCHAR(20), 
-   Gender VARCHAR(10), 
-   Conatct_no VARCHAR(20), 
-   Rating INT, 
-   Age INT, 
-   PRIMARY KEY (Driver_id) 
-);
+insert into College values('c102','DTU','Delhi','dtu@gmail.com','2000');
 
-CREATE TABLE  TRIP_DETAILS ( 
-   Trip_id INT NOT NULL, 
-   Trip_date DATE, 
-   Trip_amt DECIMAL(10,2), 
-   Driver_id INT, 
-   Usr_id INT, 
-   Taxi_id INT, 
-   Strt_time TIMESTAMP, 
-   End_time TIMESTAMP, 
-   PRIMARY KEY (Trip_id) 
-);
+select * from College 
+;
 
-CREATE TABLE BILL_DETAILS ( 
-   Bill_no INT NOT NULL, 
-   Bill_date DATE, 
-   Advance_amt DECIMAL(10,2), 
-   Discount_amt DECIMAL(10,2), 
-   Total_amt DECIMAL(10,2), 
-   Usr_id INT, 
-   Trip_id INT, 
-   PRIMARY KEY (Bill_no), 
-   UNIQUE (Trip_id) 
-);
+CREATE TABLE Student(StudentID VARCHAR(255) NOT NULL, StudentName VARCHAR(255), StudentYear INT, 
+StudentSex VARCHAR(255), StudentEmail VARCHAR(255), StudentWorkProfile VARCHAR(255), CollegeID VARCHAR(255), 
+PRIMARY KEY(StudentID), FOREIGN KEY(CollegeID) REFERENCES College(CollegeID) );
 
-CREATE TABLE  CUSTOMER_SERVICE ( 
-   Emp_id INT NOT NULL, 
-   F_name VARCHAR(20), 
-   L_name VARCHAR(20), 
-   PRIMARY KEY (Emp_id) 
-);
+insert into Student values('s100','Hardik',2,'M','hardikvats@yahoo.in','SDE','c101');
 
-CREATE TABLE  FEEDBACK ( 
-   Fbk_id INT NOT NULL, 
-   Message VARCHAR(140), 
-   Email VARCHAR(50), 
-   Emp_id INT, 
-   Usr_id INT, 
-   Trip_id INT, 
-   PRIMARY KEY (Fbk_id), 
-   UNIQUE (Emp_id) 
-);
+insert into Student values('s101','Jagriti',2,'F','jagritigaur02@gmail.com','SDE','c101');
 
-CREATE TABLE  OWNS ( 
-   Owner_id INT NOT NULL, 
-   No_Cars  INT, 
-   PRIMARY KEY (Owner_id) 
-);
+insert into Student values('s102','Harsh',3,'M','harsh@yahoo.in','FTE','c101');
 
-CREATE TABLE  OWNER_TAXI ( 
-   Owner_id INT NOT NULL, 
-   Taxi_id INT, 
-   PRIMARY KEY (Owner_id, Taxi_id) 
-);
+insert into Student values('s103','Vaibhav',4,'M','vaibhav@yahoo.in','SDE','c102');
 
-CREATE TABLE INDIVIDUAL ( 
-   Ssn INT NOT NULL, 
-   Name VARCHAR(20), 
-   Owner_id INT, 
-   PRIMARY KEY (Ssn) 
-);
+select * from Student;
 
-CREATE TABLE  TAXI_SERVICE_COMPANY ( 
-   Tsc_id INT NOT NULL, 
-   Tsc_name VARCHAR(20), 
-   Owner_id INT, 
-   PRIMARY KEY (Tsc_id) 
-);
+CREATE TABLE Freelancers(FreelancerID VARCHAR(255) NOT NULL, FreelancerName VARCHAR(255), 
+FreelancerEmail VARCHAR(255), FreelancerYearOfEducation INT, FreelancerExpertiseIn VARCHAR(255),  
+PRIMARY KEY(FreelancerID));
 
-ALTER TABLE TAXI ADD CONSTRAINT fketadr FOREIGN KEY (Driver_id) REFERENCES DRIVER(Driver_id) ON DELETE CASCADE;
+insert into Freelancers values('f100','Mohan','mohan@chitfund.com',9,'SDE');
 
-ALTER TABLE USER_TBL ADD CONSTRAINT fkusta FOREIGN KEY (Taxi_id) REFERENCES TAXI(Taxi_id) ON DELETE CASCADE;
+insert into Freelancers values('f101','Rohan','rohan@google.com',2,'FTE');
 
-ALTER TABLE TRIP_DETAILS ADD CONSTRAINT fktddr FOREIGN KEY (Driver_id) REFERENCES DRIVER(Driver_id) ON DELETE CASCADE;
+select * from Freelancers;
 
-ALTER TABLE TRIP_DETAILS ADD CONSTRAINT fktdusr FOREIGN KEY (Usr_id) REFERENCES USER_TBL(Usr_id) ON DELETE CASCADE;
+CREATE TABLE CompanyDetails(CompanyID VARCHAR(255) NOT NULL,  
+CompanyName VARCHAR(255), CompanyAddress VARCHAR(255), PRIMARY KEY(CompanyID));
 
-ALTER TABLE TRIP_DETAILS ADD CONSTRAINT fktdtax FOREIGN KEY (Taxi_id) REFERENCES TAXI(Taxi_id) ON DELETE CASCADE;
+insert into CompanyDetails values('comp100', 'chitfund', 'gurugram');
 
-ALTER TABLE BILL_DETAILS ADD CONSTRAINT fkbdtd FOREIGN KEY (Trip_id) REFERENCES TRIP_DETAILS(Trip_id) ON DELETE CASCADE;
+insert into CompanyDetails values('comp101', 'google', 'Noida');
 
-ALTER TABLE BILL_DETAILS ADD CONSTRAINT fkbdusr FOREIGN KEY (Usr_id) REFERENCES USER_TBL(Usr_id) ON DELETE CASCADE;
+select * from CompanyDetails;
 
-ALTER TABLE FEEDBACK ADD CONSTRAINT fkfbemp FOREIGN KEY (Emp_id) REFERENCES CUSTOMER_SERVICE(Emp_id) ON DELETE CASCADE;
+CREATE TABLE CompanyHrDetails( HrID VARCHAR(255) NOT NULL, HrName VARCHAR(255), 
+HrSex VARCHAR(255), CompanyID VARCHAR(255), PRIMARY KEY(HrID), 
+FOREIGN KEY(CompanyID) REFERENCES CompanyDetails(CompanyID));
 
-ALTER TABLE FEEDBACK ADD CONSTRAINT fkfbtd FOREIGN KEY (Trip_id) REFERENCES TRIP_DETAILS(Trip_id) ON DELETE CASCADE;
+insert into CompanyHrDetails values('h100', 'Ramesh', 'M', 'comp100');
 
-ALTER TABLE FEEDBACK ADD CONSTRAINT fkfbusr FOREIGN KEY (Usr_id) REFERENCES USER_TBL(Usr_id) ON DELETE CASCADE;
+insert into CompanyHrDetails values('h101', 'Suresh', 'M', 'comp100');
 
-ALTER TABLE OWNER_TAXI ADD CONSTRAINT fkeowtax FOREIGN KEY (Taxi_id) REFERENCES TAXI(Taxi_id) ON DELETE CASCADE;
+insert into CompanyHrDetails values('h102', 'Mahesh', 'M', 'comp101');
 
-ALTER TABLE OWNER_TAXI ADD CONSTRAINT fkeowowns FOREIGN KEY (Owner_id) REFERENCES OWNS(Owner_id) ON DELETE CASCADE;
+insert into CompanyHrDetails values('h103', 'Jayesh', 'M', 'comp101');
 
-ALTER TABLE INDIVIDUAL ADD CONSTRAINT fkeinowns FOREIGN KEY (Owner_id) REFERENCES OWNS(Owner_id) ON DELETE CASCADE;
+select * from CompanyHrDetails;
 
-ALTER TABLE TAXI_SERVICE_COMPANY ADD CONSTRAINT fketscowns FOREIGN KEY (Owner_id) REFERENCES OWNS(Owner_id) ON DELETE CASCADE;
+CREATE TABLE JobDetails(HrID VARCHAR(255), JobProfile VARCHAR(255), 
+FOREIGN KEY(HrID) REFERENCES CompanyHrDetails(HrID), PRIMARY KEY(HrID,JobProfile));
 
-INSERT INTO DRIVER VALUES(1,'Abhi','Gowda','Male','4693805870',5,25);
+insert into JobDetails values('h100','SDE');
 
-INSERT INTO TAXI VALUES(1,'KA-15R-3367','BENZE 300',to_date('01/01/2023','mm/dd/yyyy'),'SUV','Available',1);
+insert into JobDetails values('h101','FTE');
 
-INSERT INTO USER_TBL VALUES(1,'-','Relan','123456','Male','MCCAllum','1');
+insert into JobDetails values('h100','Analyst');
 
-INSERT INTO TRIP_DETAILS VALUES(1,to_date('01/01/2023','mm/dd/yyyy'),123,1,1,1,TO_TIMESTAMP('2013-01-01 06:14:00', 'YYYY-MM-DD HH24:MI:SS'),TO_TIMESTAMP('2023-01-01 08:14:00', 'YYYY-MM-DD HH24:MI:SS'));
+insert into JobDetails values('h102','MTS');
 
-INSERT INTO BILL_DETAILS VALUES(1,to_date('01/01/2023','mm/dd/yyyy'),1000.10,20.11,null,1,1);
+select * from JobDetails;
 
-INSERT INTO CUSTOMER_SERVICE VALUES(1,'prashuk','ajmera');
+CREATE TABLE FreeLancerHrRelation(HrID  VARCHAR(255), FreelancerID VARCHAR(255), 
+FOREIGN KEY(HrID) REFERENCES CompanyHrDetails(HrID),  
+FOREIGN KEY(FreelancerID) REFERENCES Freelancers(FreelancerID),  
+PRIMARY KEY(HrID,FreelancerID));
 
-INSERT INTO CUSTOMER_SERVICE VALUES(2,'abhi','gowda');
+insert into FreeLancerHrRelation values('h100','f100');
 
-INSERT INTO FEEDBACK VALUES(1,'good','prashuk.ajmera@gmail.com',1,1,1);
+insert into FreeLancerHrRelation values('h102','f100');
 
-INSERT INTO OWNS VALUES(1,1);
+insert into FreeLancerHrRelation values('h101','f101');
 
-INSERT INTO OWNER_TAXI VALUES (1,1);
+insert into FreeLancerHrRelation values('h103','f101');
 
-INSERT INTO INDIVIDUAL VALUES(123,'abhi owner ind',1);
+select * from FreeLancerHrRelation;
 
-CREATE OR REPLACE PROCEDURE BOOK_TAXI 
-( Name IN VARCHAR2 
-, v_Address IN VARCHAR2 
-, v_Contact IN VARCHAR2 
-, Taxi_Model IN VARCHAR2 
-, v_Gender IN VARCHAR2 
-, Advance IN decimal 
-) 
-AS 
-BEGIN 
-DECLARE  
-v_usr_id INT :=-1; 
-v_Trip_id INT :=-1; 
-v_Bill_no INT :=-1; 
-v_Taxi_id INT :=-1; 
-v_Driver_id INT :=1; 
-BEGIN 
-select MAX(Usr_id)+1 into v_usr_id from USER_TBL ; 
-select MAX(Trip_id)+1 into v_Trip_id from TRIP_DETAILS ; 
-select MAX(Bill_no)+1 into v_Bill_no from BILL_DETAILS ; 
-select taxi_id, Driver_id  into v_Taxi_id,v_Driver_id from TAXI  where Status = 'Available' and Taxi_Model = Taxi_Model; 
+CREATE TABLE CollegeCompanyRelation( CollegeID VARCHAR(255), HrID VARCHAR(255), 
+FOREIGN KEY(CollegeID) REFERENCES College(CollegeID), 
+FOREIGN KEY(HrID) REFERENCES CompanyHrDetails(HrID), PRIMARY KEY(CollegeID,HrID));
+
+insert into CollegeCompanyRelation values('c101','h100');
+
+insert into CollegeCompanyRelation values('c102','h102');
+
+insert into CollegeCompanyRelation values('c102','h101');
+
+select * from CollegeCompanyRelation;
+
+
+
+--display student details
+
+DECLARE 
+    -- Declare a cursor to retrieve student details 
+    CURSOR c1 IS 
+        SELECT s.StudentID, s.StudentName, s.StudentYear, s.StudentSex, s.StudentEmail, s.StudentWorkProfile, c.CompanyName, j.JobProfile 
+        FROM Student s 
+        JOIN CollegeCompanyRelation r ON s.CollegeID = r.CollegeID 
+        JOIN CompanyHrDetails h ON r.HrID = h.HrID 
+        JOIN CompanyDetails c ON h.CompanyID = c.CompanyID 
+        JOIN JobDetails j ON h.HrID = j.HrID 
+        WHERE c.CompanyID = 1; -- Replace 1 with the desired company ID 
  
-INSERT INTO USER_TBL values(v_usr_id, SUBSTR (Name, 1, INSTR(Name,' ',1)),SUBSTR (Name, INSTR(Name,' ',1)+1,LENGTH(Name)),v_Contact,v_Gender,v_Address,v_Taxi_id); 
-INSERT INTO TRIP_DETAILS values(v_Trip_id,sysdate, 50,v_Driver_id,v_usr_id,v_Taxi_id,sysdate,null); 
-INSERT INTO BILL_DETAILS values(v_Bill_no,null,Advance,null,null,v_usr_id,v_Trip_id); 
+    -- Declare a variable to store retrieved student details 
+    student_rec c1%ROWTYPE; 
+BEGIN 
+    -- Open the cursor and loop through the retrieved records 
+    OPEN c1; 
+    LOOP 
+        FETCH c1 INTO student_rec; 
+        EXIT WHEN c1%NOTFOUND; 
  
-END ; 
+        -- Display the retrieved student details 
+        DBMS_OUTPUT.PUT_LINE('Student ID: ' || student_rec.StudentID); 
+        DBMS_OUTPUT.PUT_LINE('Name: ' || student_rec.StudentName); 
+        DBMS_OUTPUT.PUT_LINE('Year: ' || student_rec.StudentYear); 
+        DBMS_OUTPUT.PUT_LINE('Sex: ' || student_rec.StudentSex); 
+        DBMS_OUTPUT.PUT_LINE('Email: ' || student_rec.StudentEmail); 
+        DBMS_OUTPUT.PUT_LINE('Work Profile: ' || student_rec.StudentWorkProfile); 
+        DBMS_OUTPUT.PUT_LINE('Company Name: ' || student_rec.CompanyName); 
+        DBMS_OUTPUT.PUT_LINE('Job Profile: ' || student_rec.JobProfile); 
+        DBMS_OUTPUT.PUT_LINE('---------------------------------------'); 
+    END LOOP; 
+    CLOSE c1; 
+EXCEPTION 
+    -- Handle any exceptions that might occur 
+    WHEN OTHERS THEN 
+        DBMS_OUTPUT.PUT_LINE('Error: ' || SQLERRM); 
+END; 
+ 
+
+
+-- List of students with their expertise and college
+
+DECLARE 
+  v_college_name College.CollegeName%TYPE; 
+  v_expertise Freelancers.FreelancerExpertiseIn%TYPE; 
+  v_student_name Student.StudentName%TYPE; 
+  CURSOR c2 IS  
+    SELECT s.StudentName, f.FreelancerExpertiseIn, c.CollegeName  
+    FROM Student s  
+    JOIN college c ON s.CollegeID = c.CollegeID  
+    LEFT JOIN Freelancers f ON s.StudentEmail = f.FreelancerEmail; 
+BEGIN 
+  DBMS_OUTPUT.PUT_LINE('List of students with their expertise and college:'); 
+  FOR rec IN c2 LOOP 
+    v_student_name := rec.StudentName; 
+    v_expertise := rec.FreelancerExpertiseIn; 
+    v_college_name := rec.CollegeName; 
+    DBMS_OUTPUT.PUT_LINE(v_student_name || ' has expertise in ' || v_expertise || ' and belongs to ' || v_college_name); 
+  END LOOP; 
+EXCEPTION 
+  WHEN NO_DATA_FOUND THEN 
+    DBMS_OUTPUT.PUT_LINE('No records found.'); 
+  WHEN OTHERS THEN 
+    DBMS_OUTPUT.PUT_LINE('An error occurred: ' || SQLCODE || ' - ' || SQLERRM); 
+END;
+/
+
+--comaprison between colleges based on student count
+
+DECLARE 
+  v_college_name College.CollegeName%TYPE; 
+  v_student_count College.CollegeStudentCount%TYPE; 
+  v_max_count College.CollegeStudentCount%TYPE; 
+  v_min_count College.CollegeStudentCount%TYPE; 
+  CURSOR c3 IS  
+    SELECT CollegeName, CollegeStudentCount  
+    FROM College; 
+BEGIN 
+  DBMS_OUTPUT.PUT_LINE('Comparison between colleges:'); 
+  OPEN c3; 
+  FETCH c3 INTO v_college_name, v_student_count; 
+  v_max_count := v_student_count; 
+  v_min_count := v_student_count; 
+  WHILE c3%FOUND LOOP 
+    IF v_student_count > v_max_count THEN 
+      v_max_count := v_student_count; 
+    END IF; 
+    IF v_student_count < v_min_count THEN 
+      v_min_count := v_student_count; 
+    END IF; 
+    FETCH c3 INTO v_college_name, v_student_count; 
+  END LOOP; 
+  CLOSE c3; 
+  DBMS_OUTPUT.PUT_LINE('College with the most students: ' || v_max_count); 
+  DBMS_OUTPUT.PUT_LINE('College with the least students: ' || v_min_count); 
+EXCEPTION 
+  WHEN NO_DATA_FOUND THEN 
+    DBMS_OUTPUT.PUT_LINE('No records found.'); 
+  WHEN OTHERS THEN 
+    DBMS_OUTPUT.PUT_LINE('An error occurred: ' || SQLCODE || ' - ' || SQLERRM); 
 END; 
 /
 
-Execute BOOK_TAXI('Gupta','Meerut',469380,'BENZE 300','FEMALE',19)
+--comparison between colleges for how may students are freelancers and how many are working in a company
 
-
-Execute BOOK_TAXI('Singh,'Lucknow',469380,'BENZE 300','MALE',20) 
-
-
-Execute BOOK_TAXI('Singh','Lucknow',469380,'BENZE 300','MALE',20)
-
-
-Select * From USER_TBL;
-
-Select * From TRIP_DETAILS;
-
-Select * From BILL_DETAILS;
-
-Execute BOOK_TAXI('Singh','Lucknow',469380,'BENZE 300','MALE',20) 
-
-
-Execute BOOK_TAXI('Bareja','Delhi',469380,'BENZE 300','FEMALE',20)
-
-
-Select * From USER_TBL;
-
-Select * From TRIP_DETAILS;
-
-Select * From BILL_DETAILS;
-
-Select * From TRIP_DETAILS;
-
-Select * From BILL_DETAILS;
-
-CREATE OR REPLACE PROCEDURE TRIP_END(v_trip IN INT , v_discount IN Decimal ) 
-AS 
+DECLARE 
+  v_college_name College.CollegeName%TYPE; 
+  v_freelancer_count NUMBER; 
+  v_company_count NUMBER; 
+  CURSOR c3 IS  
+    SELECT CollegeName, COUNT(CASE WHEN StudentWorkProfile = 'FTE' THEN 1 ELSE NULL END) AS freelancer_count,  
+           COUNT(CASE WHEN StudentWorkProfile != 'FTE' THEN 1 ELSE NULL END) AS company_count 
+    FROM Student 
+    JOIN College ON Student.CollegeID = College.CollegeID 
+    GROUP BY CollegeName; 
 BEGIN 
-DECLARE  
-v_total_time INT := -1; 
-v_bill_no INT :=-1; 
-BEGIN 
-select extract(day from (sysdate - Strt_time))*24 + extract(hour from (sysdate - Strt_time))   into v_total_time from TRIP_DETAILS where Trip_id = v_trip; 
- 
-update TRIP_DETAILS set End_time = sysdate where Trip_id = Trip_id ; 
-update BILL_DETAILS set Bill_date = sysdate , Discount_amt = v_discount ,Total_amt = (v_total_time * 15) - v_discount where Trip_id = v_trip  ; 
-END ; 
-END ; 
-/
-
-Execute TRIP_END(4,-10)
-
-
-Select * From TRIP_DETAILS;
-
-Select * From BILL_DETAILS;
-
-CREATE OR REPLACE TRIGGER UPDATE_DRIVER_RATING  
-AFTER INSERT  ON FEEDBACK  
-FOR EACH ROW  
-WHEN (NEW.Message like '%Bad Driver%' )  
-DECLARE  
-   v_driver_id INT;  
-BEGIN  
-   select driver_id into v_driver_id from TRIP_DETAILS where trip_id = :NEW.Trip_id; 
-    
-   update DRIVER set Rating = Rating -1 where   driver_id = v_driver_id; 
-END;  
-/
-
-Select * from driver, feedback;
-
-CREATE OR REPLACE TRIGGER  ADD_NO_OF_CARS  
-BEFORE INSERT OR UPDATE ON OWNS 
-FOR EACH ROW  
-DECLARE  
-   v_no_of_cars INT;  
-BEGIN 
-   select count(Taxi_id) into v_no_of_cars from OWNER_TAXI where Owner_id = :NEW.Owner_id group by Owner_id; 
-   :NEW.No_Cars := v_no_of_cars; 
+  DBMS_OUTPUT.PUT_LINE('Comparison between colleges for students who are freelancers and in companies:'); 
+  OPEN c3; 
+  FETCH c3 INTO v_college_name, v_freelancer_count, v_company_count; 
+  WHILE c3%FOUND LOOP 
+    DBMS_OUTPUT.PUT_LINE(v_college_name || ': Freelancers = ' || v_freelancer_count || ', Companies = ' || v_company_count); 
+    FETCH c3 INTO v_college_name, v_freelancer_count, v_company_count; 
+  END LOOP; 
+  CLOSE c3; 
+EXCEPTION 
+  WHEN NO_DATA_FOUND THEN 
+    DBMS_OUTPUT.PUT_LINE('No records found.'); 
+  WHEN OTHERS THEN 
+    DBMS_OUTPUT.PUT_LINE('An error occurred: ' || SQLCODE || ' - ' || SQLERRM); 
 END; 
 /
 
-INSERT INTO TAXI VALUES(2,'KA-15R-3368','BENZE 300',to_date('01/01/2023','mm/dd/yyyy'),'SUV','Available',1);
+--number of female workers in each college
 
-INSERT INTO OWNER_TAXI values (1,2);
+DECLARE 
+  v_college_name College.CollegeName%TYPE; 
+  v_female_worker_count NUMBER; 
+  CURSOR c3 IS  
+    SELECT CollegeName, COUNT(*) AS female_worker_count 
+    FROM Student 
+    JOIN College ON Student.CollegeID = College.CollegeID 
+    WHERE StudentSex = 'F' 
+    GROUP BY CollegeName; 
+BEGIN 
+  DBMS_OUTPUT.PUT_LINE('Number of female workers in each college:'); 
+  OPEN c3; 
+  FETCH c3 INTO v_college_name, v_female_worker_count; 
+  WHILE c3%FOUND LOOP 
+    DBMS_OUTPUT.PUT_LINE(v_college_name || ': ' || v_female_worker_count); 
+    FETCH c3 INTO v_college_name, v_female_worker_count; 
+  END LOOP; 
+  CLOSE c3; 
+EXCEPTION 
+  WHEN NO_DATA_FOUND THEN 
+    DBMS_OUTPUT.PUT_LINE('No records found.'); 
+  WHEN OTHERS THEN 
+    DBMS_OUTPUT.PUT_LINE('An error occurred: ' || SQLCODE || ' - ' || SQLERRM); 
+END; 
+/
 
-SELECT * From OWNER_TAXI;
-
-SELECT * From OWNS;
-
+--details of students who are freelancers
+DECLARE 
+  v_freelancer_id Freelancers.FreelancerID%TYPE; 
+  v_freelancer_name Freelancers.FreelancerName%TYPE; 
+  v_email Freelancers.FreelancerEmail%TYPE; 
+  v_year_of_education Freelancers.FreelancerYearOfEducation%TYPE; 
+  v_expertise Freelancers.FreelancerExpertiseIn%TYPE; 
+  v_hr_name CompanyHrDetails.HrName%TYPE; 
+  v_company_name CompanyDetails.CompanyName%TYPE; 
+  CURSOR c4 IS  
+    SELECT f.FreelancerID, f.FreelancerName, f.FreelancerEmail, f.FreelancerYearOfEducation, f.FreelancerExpertiseIn, h.HrName, d.CompanyName 
+    FROM Freelancers f 
+    JOIN FreeLancerHrRelation r ON f.FreelancerID = r.FreelancerID 
+    JOIN CompanyHrDetails h ON r.HrID = h.HrID 
+    JOIN CompanyDetails d ON h.CompanyID = d.CompanyID; 
+BEGIN 
+  DBMS_OUTPUT.PUT_LINE('Details of students who are freelancers:'); 
+  OPEN c4; 
+  FETCH c4 INTO v_freelancer_id, v_freelancer_name, v_email, v_year_of_education, v_expertise, v_hr_name, v_company_name; 
+  WHILE c4%FOUND LOOP 
+    DBMS_OUTPUT.PUT_LINE('Freelancer ID: ' || v_freelancer_id); 
+    DBMS_OUTPUT.PUT_LINE('Freelancer Name: ' || v_freelancer_name); 
+    DBMS_OUTPUT.PUT_LINE('Email: ' || v_email); 
+    DBMS_OUTPUT.PUT_LINE('Year of Education: ' || v_year_of_education); 
+    DBMS_OUTPUT.PUT_LINE('Expertise In: ' || v_expertise); 
+    DBMS_OUTPUT.PUT_LINE('HR Name: ' || v_hr_name); 
+    DBMS_OUTPUT.PUT_LINE('Company Name: ' || v_company_name); 
+    DBMS_OUTPUT.PUT_LINE('--------------------------'); 
+    FETCH c4 INTO v_freelancer_id, v_freelancer_name, v_email, v_year_of_education, v_expertise, v_hr_name, v_company_name; 
+  END LOOP; 
+  CLOSE c4; 
+EXCEPTION 
+  WHEN NO_DATA_FOUND THEN 
+    DBMS_OUTPUT.PUT_LINE('No records found.'); 
+  WHEN OTHERS THEN 
+    DBMS_OUTPUT.PUT_LINE('An error occurred: ' || SQLCODE || ' - ' || SQLERRM); 
+END; 
+/
